42e822a4cee5757dbb9ae159736bab41f6f8387b
Tunde Babatunde <34407749+fortuneship@users.noreply.github.com>
2023-02-03 07:14:10 +0100

    Update useProduct.tsx

--- src/hooks/useProduct.tsx
+++ src/hooks/useProduct.tsx
@@ -2,7 +2,7 @@ import { useContext } from "react"
 import ProductsContext from "../context/ProductsProvider"
 import { UseProductsContextType } from "../context/ProductsProvider"
 
-const useProducts = (products: any): UseProductsContextType => {
+const useProducts = (): UseProductsContextType => {
     return useContext(ProductsContext)
 }
 
